50f6275e50e59432b1aaf040af71f1fd7c494823
Maria Treesa Sebastian <mariatreesaa@gmail.com>
2018-09-28 12:15:13 +0200

    installing packages

---
+++ R/installing_packages.R
@@ -0,0 +1,13 @@
+# This R script is used for installing  and loading the required packages
+
+# Check if packages is installed, else install
+
+if("httr" %in% rownames(installed.packages()) == FALSE){
+  install.packages("httr")
+}
+if("jsonlite" %in% rownames(installed.packages()) == FALSE){
+  install.packages("jsonlite")
+}
+
+library("httr")
+library("jsonlite")
