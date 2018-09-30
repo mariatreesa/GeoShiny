2c9c4fc66babb487151f3b6001a9125c5bf32079
BMasinde <masindeb@live.com>
2018-09-30 11:50:46 +0200

    url function

--- R/api_access.R
+++ R/api_access.R
@@ -2,7 +2,16 @@
 source("R/installing_packages.R")
 
 
- geocode_apiurl <- function(path){
-   url = modify_url("https://maps.googleapis.com/maps/api/geocode/", path = path)
-   GET(url)
- }
+# geocode_apiurl <- function(path){
+#   url = modify_url("https://maps.googleapis.com/maps/api/geocode/", path = path)
+#   GET(url)
+# }
+
+# start with url function
+
+url <- function(address, return.call = "json", sensor = "false") {
+  root <- "http://maps.google.com/maps/api/geocode/"
+  u <- paste(root, return.call, "?address=", address, "&sensor=", sensor, sep = "")
+  return(URLencode(u))
+}
+
