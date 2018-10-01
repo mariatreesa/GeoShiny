8b154446d2cab68e035929b6136e484516409912
Maria Treesa Sebastian <mariatreesaa@gmail.com>
2018-10-01 11:25:44 +0200

    modification to api_access function

--- R/access_api.R
+++ R/access_api.R
@@ -10,26 +10,22 @@ source("R/installing_packages.R")
 # start with url function
 
 geocode_response <- function(address, return.call="json") {
- sample_key=""
+ sample_key="sample_key"
   root <- "https://maps.google.com/maps/api/geocode/"
   u <- paste0(root,return.call,"?address=",address,"&key=",sample_key)
   res <- GET((URLencode(u)))
   result_json <- content(res, as = "text", encoding = "UTF-8")
   result_ls <- fromJSON(result_json, flatten = FALSE)
-  return(result_ls)
+  # Extract latitude
+  lat <- result_ls$results[[3]]$location$lat
+
+  # Extract longitude
+  long <- result_ls$results[[3]]$location$lng
+
+  # Coordinates
+  return(cbind(lat, long))
 }
 
-
-response <- geocode_response("Nairobi")
-
-# Extract latitude
-lat <- response$results[[3]]$location$lat
-
-# Extract longitude
-long <- response$results[[3]]$location$lng
-
-# Coordinates
-coords <- cbind(lat, long)
 
 
 
