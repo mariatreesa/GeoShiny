7bcd5d0f3d9bec45a35ff00bfadcdc9d0954f2fd
Brian Masinde <brima748@student.liu.se>
2018-10-05 16:37:31 +0200

    no special characters conditions, stop first if no api key is given

--- R/access_api.R
+++ R/access_api.R
@@ -15,13 +15,25 @@
 # sourcing
 source("R/installing_packages.R")
 
+
 geocode_response <- function(address, map_key) {
+  # no address key, go no further
+  if(map_key == FALSE){
+    stop("No map_key")
+  }
+
   if(is.vector(address) == FALSE || length(address) > 1) {
    stop("Multiple addresses given")
   }
   if(address == ""){
    stop("Please enter an address")
   }
+
+  # No special characters
+  if(grepl('[^[:alnum:]]', address) == TRUE){
+    stop("No special characters")
+  }
+
   return.call="json"
   root <- "https://maps.google.com/maps/api/geocode/"
   u <- paste0(root,return.call,"?address=",address,"&key=",map_key)
@@ -34,8 +46,11 @@ geocode_response <- function(address, map_key) {
   # Extract longitude
   long <- result_ls$results[[3]]$location$lng
 
+
   # Coordinates
   return(c(lat, long))
 }
+
+
 
 
