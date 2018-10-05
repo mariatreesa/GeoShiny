a1f621d8d69d904b8bdef6c403b05012d4712790
Brian Masinde <brima748@student.liu.se>
2018-10-05 16:38:29 +0200

    more tests

--- tests/testthat/test_GeoShiny.R
+++ tests/testthat/test_GeoShiny.R
@@ -8,4 +8,16 @@ test_that("geocode_response takes one address at a time", {
   expect_error(geocode_response(address = ""))
 })
 
+# test that no special characters in address
+test_that("Found special characters",{
+  expect_error(geocode_response(address = "Nai#&%"))
+}
+)
+
+# test that map key is give, so that no request is sent to the api without the key
+test_that("No API",{
+  expect_error(geocode_response(address = "Nairobi", map_key = ""))
+}
+)
+
 
